34a9b2167c054cba98e6a886cea3a762abbabf24
maps4all <maps4all007@yandex.com>
2017-08-05 00:27:03 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,11 @@
+
+{
+    "moduleId" : "grabowCommuter/germany_SWR3",
+    "type" : "Website",
+    "name" : "SWR3",
+    "uri" : "http://www.swr3.de/aktuell/verkehr/stauanzeige/-/id=64076/cf=42/did=64262/ihrc2h/index.html",
+    "startTrigger" : "Stauliste",
+    "endTrigger" : "aktualisieren",
+    "startTag" : "li",
+    "endTag" : "li"
+}
