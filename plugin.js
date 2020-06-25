24136669a106839cf4e02f0b155c726864a29e55
maps4all <maps4all007@yandex.com>
2020-06-25 23:58:57 +0200

    Update plugin.js
    New trigger.

--- plugin.js
+++ plugin.js
@@ -3,8 +3,8 @@
     "moduleId" : "grabowCommuter/germany_SWR3",
     "type" : "Website",
     "name" : "SWR3",
-    "uri" : "http://www.swr3.de/aktuell/verkehr/stauanzeige/-/id=64076/cf=42/did=64262/ihrc2h/index.html",
-    "startTrigger" : "Stauliste",
+    "uri" : "https://www.swr3.de/aktuell/verkehr/meldungen-liste/index.html",
+    "startTrigger" : "Unterwegs",
     "endTrigger" : "aktualisieren",
     "startTag" : "li",
     "endTag" : "li"
